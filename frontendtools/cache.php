df1922d11150c731dc92c745ad558ea2239d9732
Chris <chris@cjmcsoftware.co.uk>
2012-03-02 03:27:11 +0000

    add file

--- frontendtools/cache.php
+++ frontendtools/cache.php
@@ -22,7 +22,7 @@ class totemcache
 		$str = file_get_contents($url);
 		//now we have it we need to process it.
 		$myFile = $this->cachedir.$name;
-		echo $myFile;
+		//echo $myFile;
 		$fh = fopen($myFile, 'w+');
 		fwrite($fh, $str);
 		fclose($fh);
