8b95f9b76640d61cf73fb5a3bfe1aa1da956f61f
Chris <chris@cjmcsoftware.co.uk>
2012-03-02 02:41:51 +0000

    add file

--- frontendtools/cache.php
+++ frontendtools/cache.php
@@ -11,7 +11,7 @@ class totemcache
 {
 	
 	//vars
-	var $cachedir = 'http://totemcms.phpfogapp.com/demo/cacheexample/cache/cache/';
+	var $cachedir = '/var/fog/apps/1101/totemcms.phpfogapp.com/frontendtools/cache';
 	var $cachetime = '24';
 	
 	/*
