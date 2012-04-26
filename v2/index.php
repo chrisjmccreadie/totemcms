0f87297323edf7edac2948383d5f793239955b13
Chris <chris@cjmcsoftware.co.uk>
2012-04-26 15:06:03 +0100

    new design

---
+++ v2/index.php
@@ -0,0 +1,97 @@
+<?php
+$base_url = '';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<!-- This is a demonstration of HTML5 goodness with healthy does of CSS3 mixed in -->
+<head>
+    <title>TOTEM CMS</title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />		
+    <!--[if IE]>
+    	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <!--[if IE 7]>
+    	<link rel="stylesheet" href="ie7.css" type="text/css" media="screen" />
+    <![endif]-->
+    <link rel="stylesheet" href="static/css/style.css" type="text/css" media="screen" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.0/jquery.min.js" type="text/javascript"></script>
+    <script type='text/javascript' src='static/js/knockout2.js'></script>
+</head>
+<body>
+<div id="container">
+	<div id="header">
+		<div class="">
+			<img src="http://www.totemsoftware.co.uk/img/logo-software.png" />
+		</div>
+		<div class="menus">
+			<div id="main-menu">
+				<ul>
+					<ul class="navigation">
+						<li><a href="index.php" target="_self" class="active">Home</a></li>
+					</ul> 
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div id="headervideo">
+		<iframe width="560" height="315" src="http://www.youtube.com/embed/Mn2p0tbdo_0" frameborder="0" allowfullscreen></iframe>
+		<div id="strapline">
+			The <span class="semi-bold">Developer's</span> CMS
+		</div>
+	</div>
+	<div id="content">
+		<h3>Access Anywhere.</h3>
+		<p class="content-text">
+			Completely web based access from any device
+		</p>
+	</div>
+	<div id="sidebar">
+		<img src="static/images/default.png" />
+	</div>
+	<div id="content">
+		<img src="static/images/default.png" />
+	</div>
+	<div id="sidebar">
+		<h3>Unlimited Sites.</h3>
+		<p class="content-text">
+			Create as many sites as you like, for FREE.
+		</p>
+	</div>
+	<div id="content">
+		<h3>Built by developer for developers</h3>
+		<p class="content-text">
+			Over 20 years of CMS experience has gone into Totem CMS.  We built it because it did not exist and we needed it.
+		</p>
+	</div>
+	<div id="sidebar">
+		<img src="static/images/default.png" />
+	</div>
+	<div id="content">
+		<img src="static/images/default.png" />
+	</div>
+	<div id="sidebar">
+		<h3>Auto Deploy.</h3>
+		<p class="content-text">
+			Automitaclly update all content on our server, or infact ANY server via FTP, SSH or GIT.  If you host with us you benefit from our world class infastructure.
+		</p>
+	</div>
+	
+</div>
+  <div id="footer">
+	</div>  
+    <script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+		try {
+		var pageTracker = _gat._getTracker("UA-9207090-1");
+		pageTracker._trackPageview();
+		} catch(err) {}
+	</script>
+	
+	
+    
+</body>
+
+</html>
